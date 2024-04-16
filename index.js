1e947b77453d14185b18a11380403617665cf2a0
Monika-Tiyyagura <monikareddytiyyagura60@gmail.com>
2024-04-15 21:33:51 -0400

    created button to roll the dice

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
+function rollDice() {
+
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-
 var randomImageSrc = "images/" + randomDiceImage;
-
 var image1 = document.querySelectorAll("img")[0];
-
 image1.setAttribute("src", randomImageSrc);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 var randomImageSrc2 = "images/" + randomDiceImage2;
-
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSrc2);
-
 
 if(randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Dimpu won...hhehehee";
@@ -23,5 +19,7 @@ else if(randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerHTML = "Babbu won....by fluke";
 }
 else {
-    document.querySelector("h1").innerHTML = "its a draw(dimpu won by 0.1%)";
+    document.querySelector("h1").innerHTML = "draw(dimpu won by 0.1%)";
+}
+
 }
